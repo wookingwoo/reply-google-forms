795709472c7b91351dfd22dac028c1b69e60fc84
JeongHeon Woo <contact@wookingwoo.com>
2021-04-27 18:32:01 +0900

    Check validity

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import data.reply
 
 driver = webdriver.Chrome(executable_path='./chromedriver_win32/chromedriver')
 
-driver.implicitly_wait(10)  # seconds
+driver.implicitly_wait(10)  # 10 seconds
 
 googleFormsURL = 'https://docs.google.com/forms/d/e/1FAIpQLScPXUcpuiFE00JLPYsABzE9tN005FSQAdQJ8A0af_POVZbi1w/viewform'
 
@@ -13,6 +13,8 @@ name = data.reply.name
 question1 = data.reply.question1
 question2 = data.reply.question2
 question3 = data.reply.question3
+txtHint = data.reply.txtHint
+autoSubmit = data.reply.autoSubmit
 
 driver.get(googleFormsURL)
 
@@ -21,39 +23,70 @@ time.sleep(0.7)
 questionRoomNum_text = driver.find_element_by_xpath(
     '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/div')
 
-print(questionRoomNum_text.text)
+if (txtHint == questionRoomNum_text.text):
+    print(questionRoomNum_text.text)
 
-questionRoomNum_input = driver.find_element_by_xpath(
-    '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    questionRoomNum_input = driver.find_element_by_xpath(
+        '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
 
-questionRoomNum_input.send_keys(roomNum)
+    questionRoomNum_input.send_keys(roomNum)
+else:
+    print("해당 답변을 찾지 못했습니다:", txtHint)
+    autoSubmit = False
 
 questionName_text = driver.find_element_by_xpath(
     '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/div')
 
-print(questionName_text.text)
+if (txtHint == questionName_text.text):
+    print(questionName_text.text)
 
-questionName_input = driver.find_element_by_xpath(
-    '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
-questionName_input.send_keys(name)
+    questionName_input = driver.find_element_by_xpath(
+        '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    questionName_input.send_keys(name)
+else:
+    print("해당 답변을 찾지 못했습니다:", txtHint)
+    autoSubmit = False
 
 question1_toggle1_text = driver.find_element_by_xpath(
     '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div/span/div/div[1]/label/div/div[2]/div/span')
-print(question1_toggle1_text.text)
 
-question1_toggle1 = driver.find_element_by_xpath('//*[@id="i13"]/div[3]/div')
-question1_toggle1.click()
+if (question1 == question1_toggle1_text.text):
+    print(question1_toggle1_text.text)
+
+    question1_toggle1 = driver.find_element_by_xpath('//*[@id="i13"]/div[3]/div')
+    question1_toggle1.click()
+else:
+    print("해당 답변을 찾지 못했습니다:", question1)
+    autoSubmit = False
 
 question2_toggle1_text = driver.find_element_by_xpath(
     '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div/span/div/div[1]/label/div/div[2]/div/span')
-print(question2_toggle1_text.text)
 
-question2_toggle1 = driver.find_element_by_xpath('//*[@id="i23"]/div[3]/div')
-question2_toggle1.click()
+if (question2 == question2_toggle1_text.text):
+    print(question2_toggle1_text.text)
+
+    question2_toggle1 = driver.find_element_by_xpath('//*[@id="i23"]/div[3]/div')
+    question2_toggle1.click()
+else:
+    print("해당 답변을 찾지 못했습니다:", question2)
+    autoSubmit = False
 
 question3_toggle1_text = driver.find_element_by_xpath(
     '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[5]/div/div/div[2]/div[1]/div/label/div/div[2]/div/span')
-print(question3_toggle1_text.text)
 
-question3_checkbox = driver.find_element_by_xpath('//*[@id="i34"]/div[2]')
-question3_checkbox.click()
+if (question3 == question3_toggle1_text.text):
+    print(question3_toggle1_text.text)
+
+    question3_checkbox = driver.find_element_by_xpath('//*[@id="i34"]/div[2]')
+    question3_checkbox.click()
+else:
+    print("해당 답변을 찾지 못했습니다:", question3)
+    autoSubmit = False
+
+if (autoSubmit):
+    print("버튼을 클릭합니다.")
+    subbitButton = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div/div/span')
+    subbitButton.click()
+
+else:
+    print("입력된 내용을 확인 후 직접 제출하세요.")
